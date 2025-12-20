08d1945aa8a5f37524157ced74dfb1273fdffda0
kevin chou <Kevinurmomchou@icloud.com>
2025-12-20 15:17:09 -0800

    m

--- src/main/java/com/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/com/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -282,14 +282,14 @@ public class SwerveSubsystem extends SubsystemBase
   public void followTrajectory(SwerveSample setpoint) {
     m_pidControllerTheta.enableContinuousInput(-Math.PI, Math.PI);
     Pose2d pose = getPose();
-    m_pidControllerX.setIZone(0.5);
-    m_pidControllerY.setIZone(0.5);
-    m_pidControllerTheta.setIZone(0.08);
+    m_pidControllerX_pose.setIZone(0.5);
+    m_pidControllerY_pose.setIZone(0.5);
+    m_pidControllerTheta_pose.setIZone(0.08);
 
     ChassisSpeeds targetSpeeds = new ChassisSpeeds( 
-      setpoint.vx + m_pidControllerX.calculate(pose.getX(), setpoint.x), 
-      setpoint.vy + m_pidControllerY.calculate(pose.getY(), setpoint.y),
-      setpoint.omega + m_pidControllerTheta.calculate(pose.getRotation().getRadians(), setpoint.heading)
+      setpoint.vx + m_pidControllerX_pose.calculate(pose.getX(), setpoint.x), 
+      setpoint.vy + m_pidControllerY_pose.calculate(pose.getY(), setpoint.y),
+      setpoint.omega + m_pidControllerTheta_pose.calculate(pose.getRotation().getRadians(), setpoint.heading)
     );
 
     // Log some basic data to see if path following is accurate.
