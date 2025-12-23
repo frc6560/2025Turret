5ab5c631760f304c0dd24ee7dd2c35610d120fed
Aneya Patel <aneya.patel@gmail.com>
2025-12-22 16:15:06 -0800

    t

--- src/main/java/com/team6560/frc2025/subsystems/HoodandFlywheel.java
+++ src/main/java/com/team6560/frc2025/subsystems/HoodandFlywheel.java
@@ -135,8 +135,7 @@ public class HoodandFlywheel extends SubsystemBase {
 
 private void configureCANCoder(CANcoder encoder) {
     CANcoderConfiguration config = new CANcoderConfiguration();
-    
-
+    config.MagnetSensor.MagnetOffset = 0.35; 
     encoder.getConfigurator().apply(config);
 }
 
@@ -315,7 +314,8 @@ private void configureHood(TalonFX motor) {
                         Math.min(HoodandFlywheelConstants.HOOD_MAX_ANGLE, degrees));
       
       // Convert to motor rotations (25:1 gear ratio)
-      double targetRotations = degrees / HoodandFlywheelConstants.HOOD_GEAR_RATIO;
+      //double targetRotations = degrees / HoodandFlywheelConstants.HOOD_GEAR_RATIO;
+      double targetRotations = (45.0/360.0) / HoodandFlywheelConstants.HOOD_GEAR_RATIO; //new
 
       double currentRotations = hoodEncoder.getPosition().getValueAsDouble();
 
